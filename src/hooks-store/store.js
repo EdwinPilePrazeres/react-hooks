7489a02fd8e49c07e7a06df859f5ab55f66309b5
Edwin Pile <69055818+EdwinPilePrazeres@users.noreply.github.com>
2020-12-26 15:56:23 -0500

    master

--- src/hooks-store/store.js
+++ src/hooks-store/store.js
@@ -4,7 +4,7 @@ let globalState = {};
 let listeners = [];
 let actions = {};
 
-export const useStore = () => {
+export const useStore = (shouldListen = true) => {
     const setState = useState(globalState)[1];
 
     const dispatch = (actionIdentifier, payload) => {
@@ -17,16 +17,22 @@ export const useStore = () => {
     }
 
     useEffect(() => {
-        listeners.push(setState);
+        if (shouldListen) {
+            listeners.push(setState);
+        }
+
 
         return () => {
-            listeners = listeners.filter(li => li !== setState);
+            if (shouldListen){
+                listeners = listeners.filter(li => li !== setState);
+            }
         }
     }, [setState]);
 
     return [globalState, dispatch];
 
 };
+
 
 export const initStore = (userActions, initialState) => {
     if (initialState) {
